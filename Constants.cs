8b18a792e1507c7c045100fbe50cbee6cfa2899f
Nemanja Jerinic <njerinic@hz-gmbh.de>
2026-05-05 16:38:31 +0200

    Initial global refactor

---
+++ Constants.cs
@@ -0,0 +1,18 @@
+namespace Game_of_live
+{
+    public static class Constants
+    {
+        public const int MAP_SIZE = 50;
+
+        public const int CELL_SIZE = 20;
+        public const int CELL_SPACING = 21;
+
+        public const int MIN_DELAY = 50;
+        public const int MAX_DELAY = 1000;
+        public const int DEFAULT_DELAY = 50;
+
+        public const int MIN_NEIGHBORS_TO_SURVIVE = 2;
+        public const int MAX_NEIGHBORS_TO_SURVIVE = 3;
+        public const int NEIGHBORS_TO_BIRTH = 3;
+    }
+}
